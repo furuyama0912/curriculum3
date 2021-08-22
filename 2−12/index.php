78306c1d814544728bf480514ac4fdadeb070797
furuyama0912 <shoichig8@gmail.com>
2021-08-22 23:05:36 +0900

    2−12

---
+++ 2−12/index.php
@@ -0,0 +1,17 @@
+<form action="result.php" method="post">
+        私の名前：
+        <input type="text" name="my_name" /><br>
+        ご希望商品：
+        <input type="radio" name="kinds" value="A賞">A賞
+        <input type="radio" name="kinds" value="B賞">B賞
+        <input type="radio" name="kinds" value="C賞">C賞<br>
+        個数：
+        <select name="number">
+            <?php for ($i=1;$i<=10;$i++){ ?>
+            <option value="<?php echo $i; ?>">
+            <?php echo $i; ?>
+            </option>
+            <?php } ?>
+        </select><br>
+        <input type="submit" value="送信" />
+    </form>
