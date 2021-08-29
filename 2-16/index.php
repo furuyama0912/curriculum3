56c646c3a6a12b8e12c0332bc3425c898932f0d7
furuyama0912 <shoichig8@gmail.com>
2021-08-30 00:39:59 +0900

    2-16

---
+++ 2-16/index.php
@@ -0,0 +1,26 @@
+<?php
+    $testFile = "test.txt";
+    $contents = "自分で行った課題です！";
+    
+    if (is_writable($testFile)) {
+        $fp = fopen($testFile, "w");
+        fwrite($fp, $contents);
+        fclose($fp);
+        echo "書き込み完了！";
+    } else {
+        echo "書き込み出来ていません。";
+        exit;
+    }
+
+
+    $test_file = "test2.txt";
+    
+    if (is_readable($test_file)) {
+        $fp = fopen($test_file, "r");
+        while ($line = fgets($fp)) {
+            echo $line.'<br>';
+        }
+    } else {
+        echo "not readable!";
+        exit;
+    }
